676e7190be04c5c5e3366644a72aa6363bad3bbf
Dedekind561 <mitchell.lloyd154@gmail.com>
2024-06-10 18:46:17 +0100

    can fetch and display title of shows

--- client/src/app/showings/page.js
+++ client/src/app/showings/page.js
@@ -1,8 +1,30 @@
+// fetching the data from somewhere
+"use client"
+import { useEffect, useState } from "react";
+
 export default function Showings() {
+
+  const [shows, setShows] = useState([]);
+  
+  useEffect(() => {
+    fetch(`http://localhost:9090/api/shows/all-open`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setShows(data.showings);
+      })
+  },[]) // <--- fetching data happens outside the component
+
   return (
     <main>
       <h1>Now showing</h1>
       <p>List the theatre showings here...</p>
+      <ul>
+        {shows.map(show => {
+          return <p>{show.title}</p>
+        })}
+      </ul>
     </main>
   );
 }
